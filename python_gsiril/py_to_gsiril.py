f9b7c158b4cf6699b40755cb64e5437fb0fa3ef0
Xavier Silva <xavierwsilva+git@gmail.com>
2025-09-02 13:38:56 -0400

    Add modes for repeating and calling positions

--- python_gsiril/py_to_gsiril.py
+++ python_gsiril/py_to_gsiril.py
@@ -10,15 +10,30 @@ def find_touches(curr_touch,lead_types,desired_changes):
   length, status = prove_touch(curr_touch)
 
   # touch is false, adding on anything would not change that fact
-  if status == False:
+  if status == False and not REPEAT_MODE:
+    return
+  
+  # in this combination of modes: check if adding actually changed the result
+  if REPEAT_MODE and CALLING_POSITIONS_MODE and curr_touch != []:
+    if prove_touch(curr_touch[:-1]) == prove_touch(curr_touch):
+      return
+    
+  if REPEAT_MODE and CALLING_POSITIONS_MODE and length > desired_changes + 200:
     return
   
   # touch is true, adding on anything would break this fact
   if status == True:
     output_touch(curr_touch)
-    return
+    if not REPEAT_MODE:
+      return
+    
+    if REPEAT_MODE and CALLING_POSITIONS_MODE:
+      return
 
-  if length >= desired_changes:
+  if not REPEAT_MODE and length >= desired_changes:
+    return
+  
+  if REPEAT_MODE and not CALLING_POSITIONS_MODE and len(curr_touch)*LEAD_LENGTH >= desired_changes:
     return
 
   for l in lead_types:
@@ -29,9 +44,14 @@ def output_touch(touch):
   global count
   count += 1
   outstr = ','.join(touch)
+
+  if REPEAT_MODE:
+    outstr = f"repeat({outstr}, {{/12345678/: break}})"
+
   with open("output.txt", "a") as f:
-    print(count, f"Outputted touch of length {len(touch)*lead_length} to file.")
-    print(count, f"Touch is {len(touch)*lead_length} changes:", outstr, file=f)
+    length, _ = prove_touch(touch)
+    print(count, f"Outputted touch of length {length} to file.")
+    print(count, f"Touch is {length} changes:", outstr, file=f)
 
 # returns if touch is true
 def prove_touch(touch):
@@ -39,11 +59,12 @@ def prove_touch(touch):
     return (0, None)
 
   subprocess.run(f"cp {method_name}_template.siril {method_name}.siril", shell=True)
-  subprocess.run(f"echo \"\nprove {','.join(touch)}\" >> {method_name}.siril", shell=True)
-  # print(f"echo \"\nprove repeat({','.join(touch)},{{/12345678/:break}}))\"")
-  # subprocess.run(f"echo \"\nprove repeat({','.join(touch)},{{/12345678/:break}})\" >> {method_name}.siril", shell=True)
+  if REPEAT_MODE:
+    subprocess.run(f"echo \"\nprove repeat({','.join(touch)},{{/12345678/:break}})\" >> {method_name}.siril", shell=True)
+  else:
+    subprocess.run(f"echo \"\nprove {','.join(touch)}\" >> {method_name}.siril", shell=True)
+  
   completed = subprocess.run(f"gsiril < {method_name}.siril", shell=True, text=True, capture_output=True)
-
   num_changes = int(completed.stdout.split()[0])
   if "Touch is true" in completed.stdout:
     return (num_changes, True)
@@ -56,7 +77,15 @@ def prove_touch(touch):
 
 method_name = "twinimus"
 subprocess.run("mv output.txt prev_output.txt", shell=True)
-lead_types = ["pl","hu"]
-lead_length = 8
+lead_types = ["pl","hu","sn"]
+LEAD_LENGTH = 8
+REPEAT_MODE = True
+CALLING_POSITIONS_MODE = True
 # 1250/8 = 156.25, so 157 leads needed
-find_touches([], lead_types, 120)
+
+if CALLING_POSITIONS_MODE:
+  lead_types = ["hM","hW","hH","eW"]
+else:
+  lead_types = ["pl","hu","sn"]
+
+find_touches([], lead_types, 1250)
